2791a4484871595b13c87b3d39565386dd686009
apple <mahdibayat72@hotmail.com>
2020-06-14 11:39:39 +0430

    Monster State build

---
+++ build/precache-manifest.432aaf7ed9998679f8f798af4ea23503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751e2eb1d3afceb93b3cbbed90388bf2",
+    "url": "/Monsters-State/index.html"
+  },
+  {
+    "revision": "68e5756f65b56b66e26e",
+    "url": "/Monsters-State/static/css/main.7ebf5b17.chunk.css"
+  },
+  {
+    "revision": "d2b2a7e2bde2095b5fca",
+    "url": "/Monsters-State/static/js/2.6227b828.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-State/static/js/2.6227b828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68e5756f65b56b66e26e",
+    "url": "/Monsters-State/static/js/main.e3159e75.chunk.js"
+  },
+  {
+    "revision": "02c3060dcab01c9e501e",
+    "url": "/Monsters-State/static/js/runtime-main.7771f6ba.js"
+  }
+]);
--- src/components/SearchBar/search-bar-component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './search-bar-component.scss'
-
-
-export const SearchBar = (props) => (
-    <div className='input-area'>
-        <input className='input' type='text' placeholder='search your killer here' value={props.input} onChange={props.handleChange} />
-    </div>
-)
